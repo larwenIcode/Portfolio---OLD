224ca9ec7098747cdf0389c8d8f9fb4eafd82b22
Larsène Lasy <larsene.lasy@outlook.fr>
2021-02-23 18:13:15 +0100

    1ière version de mon accueil

--- Common/footer.php
+++ Common/footer.php
@@ -1,3 +1,5 @@
-<footer>
-    
+<footer class="bgcolor2 text-center">
+    <h4>Page crée par Lasy Larsène<br>
+    Copyright:  -</h4>
+    <a href="#" class="fs-3">Contact</a>
 </footer>
---
+++ Pages/Main.php
@@ -0,0 +1,38 @@
+<main class="container-fluid px-5">
+    <Section class="row my-5">
+        <div class="text-center bgcolor3 border border-dark rounded-pill col-md-6 offset-3 col-sm-12 p-4">
+            <h2>Bienvenue sur mon site</h2>
+        </div>
+    </Section>
+    <section class="my-5">
+        <div class="row my-5">
+        <article class="col-md-4 col-sm-12">
+            <div class="cardtitle text-center mx-auto bgcolor3 border border-dark rounded p-2">
+            <h4>CV</h4>
+        </div>
+        </article>
+        <article class="col-md-4 col-sm-12">
+            <div class="cardtitle text-center mx-auto bgcolor3 border border-dark rounded p-2">
+            <h4>Contact</h4>
+        </div>
+        </article>
+        <article class="col-md-4 col-sm-12">
+            <div class="cardtitle text-center mx-auto bgcolor3 border border-dark rounded p-2">
+            <h4>Mes intérets</h4>
+            </div>
+        </article>
+        </div>
+        <div class="row my-5">
+        <article class="col-md-4 col-sm-12 offset-1">
+            <div class="cardtitle text-center mx-auto bgcolor3 border border-dark rounded p-2">
+            <h4>Mes Expériences</h4>
+        </div>
+        </article>
+        <article class="col-md-4 col-sm-12 offset-2">
+            <div class="cardtitle text-center mx-auto bgcolor3 border border-dark rounded p-2">
+            <h4>Mes compétences</h4>
+        </div>
+        </article>
+        </div>
+    </section>
+</main>
