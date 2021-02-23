168c72389471577d2cd137b008b516c707bbb243
Larsène Lasy <larsene.lasy@outlook.fr>
2021-02-23 10:30:52 +0100

    first commit

---
+++ .history/index_20210223101438.php
@@ -0,0 +1,15 @@
+<?php 
+session_start();
+include('utils/db.php');
+include("Common/head.php");
+$whitelist = array("Main","interets","experiences","CV","Contact","Competences");
+if (isset($_SESSION['user'])) {
+    array_push($whitelist, "Admin");
+}
+if (isset($_GET['page']) && in_array($_GET['page'], $whitelist)) {
+    include("Pages/" . $_GET['page'] . ".php");
+} else {
+    include("Pages/Main.php");
+}
+include("Common/footer.php");
+?>
